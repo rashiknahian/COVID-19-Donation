37fdd2b3165635aa3bd061757ab5537f979827e7
Rashik I Nahian <r.i.nahian@hotmail.com>
2020-03-29 20:27:36 +0600

    separate head file

--- index.php
+++ index.php
@@ -3,17 +3,7 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Rashik Ishrak Nahian (www.nahian.me)">
-
-    <title>Pandemic Crisis Response Team</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <link href="css/style.css" rel="stylesheet">
+    <?php include("head.php");?>
 
 </head>
 
