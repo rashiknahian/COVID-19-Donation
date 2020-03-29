cec6d5b55f4f88dc4698aed2baeacf104404a6c0
Rashik I Nahian <r.i.nahian@hotmail.com>
2020-03-29 19:49:16 +0600

    left side done

--- index.php
+++ index.php
@@ -3,28 +3,68 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="Rashik Ishrak Nahian (www.nahian.me)">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Rashik Ishrak Nahian (www.nahian.me)">
 
-  <title>Pandemic Crisis Response Team</title>
+    <title>Pandemic Crisis Response Team</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-  <!-- Page Content -->
-  <div class="container">
+    <!-- Page Content -->
+    <div class="container">
+        <section class="donation">
+          
+               
+           
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Title</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="">
+                        <div class="col-md-12 horizontal-button">
+                            <h1>I am a Donar</h1>
 
-  </div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="col-md-12 horizontal-button">
+                            <h1>I am Seeking Support</h1>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="col-md-12 horizontal-button">
+                            <h1>See The Total Result</h1>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-2 join">
+                    <h1>Join Us</h1>
+                </div>
+            </div>
+            <div class="col-md-6">
+                
+            </div>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.slim.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+        </section>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.slim.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
