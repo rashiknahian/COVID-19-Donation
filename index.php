c300e272ce2ca3b1026dc896fca89ba845cb99da
Rashik I Nahian <r.i.nahian@hotmail.com>
2020-03-29 20:21:38 +0600

    home page done

--- index.php
+++ index.php
@@ -30,32 +30,34 @@
                     <h1>Title</h1>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-4">
+            <div class="row button">
+                <div class="col-md-4 ">
                     <a href="">
-                        <div class="col-md-12 horizontal-button">
+                        <div class="col-md-12 horizontal-button" id="donar">
                             <h1>I am a Donar</h1>
 
                         </div>
                     </a>
                     <a href="">
-                        <div class="col-md-12 horizontal-button">
+                        <div class="col-md-12 horizontal-button" id="support">
                             <h1>I am Seeking Support</h1>
                         </div>
                     </a>
                     <a href="">
-                        <div class="col-md-12 horizontal-button">
+                        <div class="col-md-12 horizontal-button" id="result">
                             <h1>See The Total Result</h1>
                         </div>
                     </a>
                 </div>
                 <div class="col-md-2 join">
-                    <h1>Join Us</h1>
+                   
+                    <a href=""><h1>Join Us</h1></a>
                 </div>
+                <div class="col-md-6">
+                        <img src="img/bd_map.jpg" alt="" class="map">
+                    </div>
             </div>
-            <div class="col-md-6">
-                
-            </div>
+            
 
 
 
