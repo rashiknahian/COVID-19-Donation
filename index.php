cdd4d39e15fdd421dd094f60e5ad13a9b159a291
Rashik I Nahian <r.i.nahian@hotmail.com>
2020-03-29 20:26:31 +0600

    separate footer file

--- index.php
+++ index.php
@@ -65,12 +65,8 @@
 
         </section>
     </div>
-    <footer class="footer fixed-bottom">
-        <p>Copyright 2020. Pandemic Crisis Response Team</p>
-    </footer>
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.slim.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    
+    <?php include("footer.php");?>
 
 </body>
 
